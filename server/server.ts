d78b9db696ef3ef6bf851fddc4ee8e93e0b7ca1f
Johnny Reina <johnny.r.reina@gmail.com>
2019-10-11 13:25:26 -0500

    I'm dumb

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import api from './routes/Api';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
 
 const env = process.env.ENVIRONMENT || 'dev';
 const port = env === 'production' ? 80 : 4000;
+
+if(env === 'dev') dotenv.config();
 
 const server = express();
 server.use(morgan('dev'));
--- server/util/ConditionalFetch.ts
+++ server/util/ConditionalFetch.ts
@@ -5,18 +5,24 @@ const responseMap: Map<string, any> = new Map();
 
 export async function conditionalFetch<T>(uri: string): Promise<T> {
   let headers: {[x:string]: any} = {
-    'Authorization': 'token '
+    'Authorization': `token ${process.env.GITHUB_API_KEY}`
   };
   if (etagMap.has(uri)) {
     const etag = etagMap.get(uri) as string;
     headers["If-None-Match"] = etag;
   }
   const response = await fetch(uri, { headers });
+
   const status = response.status;
   if (status === 304) {
-    console.log("nothing changed");
     return responseMap.get(uri) as T;
   }
+
+  if(response.headers.has('etag')) {
+    const etag = response.headers.get('etag') as string;
+    etagMap.set(uri, etag);
+  }
+
   const data = await response.json();
 
   responseMap.set(uri, data);
